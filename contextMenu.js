72ff2810d44f630eecae7b72e996d9880a9cac68
DewaldoDev <dewaldodev@gmail.com>
2016-01-05 21:07:19 -0500

    Added additional comments

--- contextMenu.js
+++ contextMenu.js
@@ -1,8 +1,10 @@
+//Initializer for right-click context menu
 var menuItem = chrome.contextMenus.create({
 	'title': 'Investopedia Lookup',
 	'contexts': ['selection'],
 	'onclick': termLookup});
 
+//Callback function which sends the intial get requst to Investopedia
 function termLookup(info, tab){
 	var term = info.selectionText.replace(/\s+/g, '-').toLowerCase(),
 		firstLetter = term.charAt(0);
@@ -14,12 +16,16 @@ function termLookup(info, tab){
 	$.get(URL, parseResponse);
 }
 
+//Parses response text from Investopedia
 function parseResponse(responseText){
-	var definition = 'a';
+
+	//var definition = '';
 	$(responseText).find('.content-box-term').children().each(function(i, element){
+
+		//This condition handles the definition text and needs to be improved
 		if($(this).is('p')){
-			//$(this).dialog();
-			//definition.concat($(this).text());
+			//$(this).dialog(); <-- This is the method for jQuery UI dialog box but doesn't work yet
+			//definition.concat($(this).text()); <-- This is to avoid multiple alert boxes but doesn't work
 			alert($(this).text());
 		} else if ($(this).is('div#NextUpBox')){
 			return false;
